058cd8ff092b036428384ddba1e192ac1da7bf88
sambw110922 <sambw042922@gmail.com>
2022-09-15 20:54:36 +0100

    The first design.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Book Tracker' });
+});
+
+//  Read books
+router.get("/booksread", function(req, res, next){
+  
+  //  Books I've read
+  var br = [
+    {
+      "author":"JRR Tolkien",
+      "title":"The Lord of the Rings: The Fellowship of the Ring"
+    },
+    {
+      "author":"JRR Tolkien",
+      "title":"The Lord of the Rings: The Two Towers"
+    },
+    {
+      "author":"Stephen King",
+      "title":"The Outsider"
+    },
+    {
+      "author":"Stephen King",
+      "title":"The Revival"
+    },
+    {
+      "author":"Stephen King",
+      "title":"Lisey's Story"
+    },
+    {
+      "author":"Peter Robinson",
+      "title":"Before the Poison"
+    },
+  ];
+
+  res.render("booksread", { mybooks: br} );
+
+});
+
+//  The about page
+router.get("/about", function(req, res, next){
+ res.render("about");
 });
 
 module.exports = router;
