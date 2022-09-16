eff9bff9c23799b39ce1f1756c83286a762ca4bb
sambw110922 <sambw042922@gmail.com>
2022-09-16 12:45:30 +0100

    Added books owned and have, and json file

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res, next) {
 //  Read books
 router.get("/booksread", function(req, res, next){
   
+  /*
   //  Books I've read
   var br = [
     {
@@ -36,8 +37,115 @@ router.get("/booksread", function(req, res, next){
       "title":"Before the Poison"
     },
   ];
+  */
 
-  res.render("booksread", { mybooks: br} );
+    //  default book
+    var tb = [
+      {
+        "bookID" : 0,
+        "bookTitle" : "DEFAULT",
+        "bookAuthor" : "DEFAULT",
+        "isOwned" : false,
+        "hasRead" : false
+      }
+    ];
+  
+    //  get books
+    const bookdata = require("../public/json/totalbooks.json")
+   
+    tb = bookdata;
+  
+    //  Books owned
+    var br = [];
+  
+    for(let i = 0; i < bookdata.booklist.length; i++){
+      
+      if(bookdata.booklist[i].hasRead == true){
+        br.push(bookdata.booklist[i]);
+      }
+  
+    }
+
+  res.render("booksread", { booksread: br} );
+
+});
+
+//  The books that I own.
+router.get("/booksowned", function(req, res, next){
+
+  /*
+  //  Books that I own.
+  var bo = [
+    {
+      "author":"JRR Tolkien",
+      "title":"The Hobbit"
+    },
+    {
+      "author":"JRR Tolkien",
+      "title":"The Lord of the Rings: The Fellowship of the Ring"
+    },
+    {
+      "author":"JRR Tolkien",
+      "title":"The Lord of the Rings: The Two Towers"
+    },
+    {
+      "author":"JRR Tolkien",
+      "title":"The Lord of the Rings: The Two Towers"
+    },
+  ];
+
+  */
+
+  //  default book
+  var tb = [
+    {
+      "bookID" : 0,
+      "bookTitle" : "DEFAULT",
+      "bookAuthor" : "DEFAULT",
+      "isOwned" : false,
+      "hasRead" : false
+    }
+  ];
+
+  //  get books
+  const bookdata = require("../public/json/totalbooks.json")
+ 
+  tb = bookdata;
+
+  //  Books owned
+  var bo = [];
+
+  for(let i = 0; i < bookdata.booklist.length; i++){
+    
+    if(bookdata.booklist[i].isOwned == true){
+      bo.push(bookdata.booklist[i]);
+    }
+
+  }
+
+  res.render("bookshave", { booksowned:  bo } );
+
+
+});
+
+//  Total books
+router.get("/totalbooks", function(req, res, next){
+
+  var tb = [
+    {
+      "bookID" : 0,
+      "bookTitle" : "DEFAULT",
+      "bookAuthor" : "DEFAULT",
+      "isOwned" : false,
+      "hasRead" : false
+    }
+  ];
+
+  const bookdata = require("../public/json/totalbooks.json")
+ 
+  tb = bookdata;
+
+  res.render("totalbooks", { totalBooks : tb });
 
 });
 
